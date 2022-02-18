d44e88e70c8a7fde2869285765dd7172c5ea1201
Gilberto Junior <gagjunior@hotmail.com>
2022-02-18 03:15:42 -0300

    Criando a appBar

---
+++ lib/pages/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+          '\$ Conversor \$',
+        ),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: ,
+    );
+  }
+}
