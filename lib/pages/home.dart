b1c2b7dc86315d5c40b5f0f9b194048b8e5a0219
Gilberto Junior <gagjunior@hotmail.com>
2022-02-18 22:54:16 -0300

    Criar a interface

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double? dolar;
+  double? euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,8 +50,73 @@ class _HomeState extends State<Home> {
                   textAlign: TextAlign.center,
                 ));
               } else {
-                return Container(
-                  color: Colors.green,
+                dolar =
+                    snapshot.data!["results"]["currencies"]["USD"]["buy"] ?? 0;
+                euro =
+                    snapshot.data!["results"]["currencies"]["EUR"]["buy"] ?? 0;
+                return SingleChildScrollView(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Icon(
+                        Icons.monetization_on,
+                        size: 150,
+                        color: Colors.amber,
+                        
+                      ),
+                      TextField(
+                        style: const TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Reais',
+                          labelStyle: const TextStyle(
+                            color: Colors.amber,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          prefixText: 'R\$ ',
+                        ),
+                      ),
+                      const Divider(),
+                      TextField(
+                        style: const TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Doláres',
+                          labelStyle: const TextStyle(
+                            color: Colors.amber,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          prefixText: 'US\$ ',
+                        ),
+                      ),
+                      const Divider(),
+                      TextField(
+                        style: const TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Euros',
+                          labelStyle: const TextStyle(
+                            color: Colors.amber,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          prefixText: '€ ',
+                        ),
+                      )
+                    ],
+                  ),
                 );
               }
           }
